6db0d9711e1f1406a1befd7773d5313a3344afda
Danny Gratzer <jozefg@cmu.edu>
2015-04-05 12:55:14 -0400

    Stubbed out the package

--- wiki-summary.el
+++ wiki-summary.el
@@ -1,10 +1,21 @@
 (require 'url)
+(require 'json)
 
 (defun wiki-summary/make-api-query (s)
+  "Given a wiki page title, generate the url for the API call
+   to get the page info"
   (let ((pre "http://en.wikipedia.org/w/api.php?continue=&action=query&titles=")
         (post "&prop=extracts&exintro=&explaintext=&format=json&redirects")
         (term (replace-regexp-in-string " " "_" s)))
     (concat pre term post)))
+
+(defun wiki-summary/extract-summary-from-response (resp)
+  "Given the JSON reponse from the webpage, grab the summary as a string"
+  'todo)
+
+(defun wiki-summary/format-summary-in-buffer (summary)
+  "Given a summary, stick it in the *wiki-summary* buffer and display the buffer"
+  'todo)
 
 (defun wiki-summary (s)
   "Return the wikipedia page's summary for a term"
