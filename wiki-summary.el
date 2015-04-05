a68451eb437383f82aa98faade2123815317126f
Danny Gratzer <jozefg@cmu.edu>
2015-04-05 13:21:20 -0400

    Fleshed out a toy implemenation. Display window needs work

--- wiki-summary.el
+++ wiki-summary.el
@@ -4,18 +4,30 @@
 (defun wiki-summary/make-api-query (s)
   "Given a wiki page title, generate the url for the API call
    to get the page info"
-  (let ((pre "http://en.wikipedia.org/w/api.php?continue=&action=query&titles=")
+  (let ((pre "https://en.wikipedia.org/w/api.php?continue=&action=query&titles=")
         (post "&prop=extracts&exintro=&explaintext=&format=json&redirects")
         (term (replace-regexp-in-string " " "_" s)))
     (concat pre term post)))
 
 (defun wiki-summary/extract-summary-from-response (resp)
   "Given the JSON reponse from the webpage, grab the summary as a string"
-  'todo)
+  (let* ((query (plist-get resp 'query))
+         (pages (plist-get query 'pages))
+         (info (cadr pages))
+         (summary (plist-get info 'extract)))
+    (if (not summary)
+        (message "No page found!")
+      summary)))
 
 (defun wiki-summary/format-summary-in-buffer (summary)
   "Given a summary, stick it in the *wiki-summary* buffer and display the buffer"
-  'todo)
+  (let ((buf (generate-new-buffer "*wiki-summary*")))
+    (with-current-buffer buf
+      (princ summary buf)
+      (fill-paragraph))
+    (split-window-horizontally)
+    (other-window 1)
+    (switch-to-buffer buf)))
 
 (defun wiki-summary (s)
   "Return the wikipedia page's summary for a term"
@@ -26,7 +38,10 @@
        (let ((json-object-type 'plist)
              (json-key-type 'symbol)
              (json-array-type 'vector))
-         (let ((result (json-read)))
-           (message "Made it here"))))))
+         (let* ((result (json-read))
+                (summary (wiki-summary/extract-summary-from-response result)))
+           (wiki-summary/format-summary-in-buffer summary))))))
+
+(wiki-summary "Emacs")
 
 (provide 'wiki-summary)
